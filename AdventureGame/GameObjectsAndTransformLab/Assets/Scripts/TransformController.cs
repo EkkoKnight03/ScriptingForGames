63969a08260d703216498bf5be2b6493b0588156
Cody Jensen <codyljensen03@gmail.com>
2024-09-19 16:56:38 -0600

    Game Object Transformation Challenge

--- AdventureGame/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ AdventureGame/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -7,10 +7,10 @@ public class TransformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var x = Mathf.PingPong(Time.time * 15, 3);
+        var p = new Vector3(x, 0, 0);
         transform.position = p;
 
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(1200, 1200, 1200) * Time.deltaTime);
     } 
 }
